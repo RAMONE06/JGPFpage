ad3ac9b8479ead932e3cb25e23baea39dd697789
RAMONE06 <158297933+RAMONE06@users.noreply.github.com>
2025-09-25 16:29:19 +0100

    added button and image

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+function myFunction(){
+    console.log("arrived at myFunction")
+    window.location = "https://shop.pinkfloyd.com/?srsltid=AfmBOopCH7YruDeacrWGPSbCjWX-CJe8oohBxrdk5Qhe900WxjlfQbPP"
+}
