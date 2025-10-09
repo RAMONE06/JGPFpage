c5736b749e02fbb1bb52d84ce4683e4ca9662b42
RAMONE06 <158297933+RAMONE06@users.noreply.github.com>
2025-10-09 16:58:56 +0100

    and thing

---
+++ JS/fundamentals.js
@@ -0,0 +1,20 @@
+/********************
+*   Fundamantals
+********************/
+
+// Window alert
+function initialized()
+{
+window.alert("Hello World");
+
+document.getElementById('printhere').innerHTML = "Hello World";
+
+let x = "20" + "50"
+Document.getElementById('datatypes').innerHTML = x;
+} b
+
+//Funtion
+function dateTime()
+{
+    document.getElementById('datetime').innerHTML = Date();
+}
--- week2/js/main.js
+++
@@ -1,4 +0,0 @@
-function myFunction(){
-    console.log("arrived at myFunction")
-    window.location = "https://shop.pinkfloyd.com/?srsltid=AfmBOopCH7YruDeacrWGPSbCjWX-CJe8oohBxrdk5Qhe900WxjlfQbPP"
-}
